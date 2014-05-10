2d1dd9cd5f971b9c151f18b275e71059b3a31955
anetfreak <anetfreak@gmail.com>
2014-05-10 16:40:21 -0700

    Shellscript update,..

--- bin/run.sh
+++ bin/run.sh
@@ -5,13 +5,13 @@
 
 # working direct to set the path to our modules (not 
 # the most efficient but it works)
-export MOO_HOME="$( cd "$( dirname "${BASH_SOURCE[0]}/"/)" && cd .. && pwd )"
+export PIN_HOME="$( cd "$( dirname "${BASH_SOURCE[0]}/"/)" && cd .. && pwd )"
 
-echo -e "\n** starting service from $MOO_HOME **\n"
+echo -e "\n** starting service from $PIN_HOME **\n"
 
 # configuration
-export PYTHONPATH=${MOO_HOME}/moo:${PYTHONPATH}
+export PYTHONPATH=${PIN_HOME}/moo:${PYTHONPATH}
 
 # run
-#python ${MOO_HOME}/moo/moo.py ${MOO_HOME} ${MOO_HOME}/conf/moo.conf
-python ${MOO_HOME}/moo/run.py ${MOO_HOME} ${MOO_HOME}/conf/moo.conf
+#python ${PIN_HOME}/moo/moo.py ${PIN_HOME} ${PIN_HOME}/conf/moo.conf
+python ${PIN_HOME}/moo/run.py ${PIN_HOME} ${PIN_HOME}/conf/moo.conf
