e5e901eb2e9fd7eb6d07aa9cdfcee6d1552eea73
amitsprofile <amit.agrawal@sjsu.edu>
2014-05-11 17:57:47 -0700

    board db database completed

--- moo/board_dao.py
+++ moo/board_dao.py
@@ -16,12 +16,13 @@ class DBConn():
         uri = "/boarddetails"
         request = "curl -X PUT http://" + self.host + ":" + str(self.port) + uri
         response = os.popen(request).read()
+        return "Success: Create DB Connection"
 
     def createBoard(self,userId, boardName, boardDesc, category, isPrivate):
         #Get previous Board record for this userId from DB
         request = 'curl -X GET -H "Accept: application/json" http://127.0.0.1:5984/userboards/'+str(userId)
         response = os.popen(request).read()
-        print 'current records ',response
+        #print 'current records ',response
         res = json.loads(response)
         prev_boards = None     #is a list of boards
         rev = None             #is a _rev returned in query
@@ -56,9 +57,9 @@ class DBConn():
             request = "curl -i -H 'Accept: application/json' -H 'Content-Type: application/json' --data '" +json.dumps(data)+ "' http://" + self.host + ":" + str(self.port) + uri
 
         #Execute the DB update request
-        print 'New request is ',request
+        #print 'New request is ',request
         response = os.popen(request).read()
-        print 'Response is: ', response
+        #print 'Response is: ', response
 
         #Insert Board Details in table boardDetails
         uri = "/boarddetails"
@@ -69,9 +70,10 @@ class DBConn():
         data["category"] = category
         data["isPrivate"] = isPrivate
         request = "curl -i -H 'Accept: application/json' -H 'Content-Type: application/json' --data '" +json.dumps(data)+ "' http://" + self.host + ":" + str(self.port) + uri
-        print 'New request is ',request
+        #print 'New request is ',request
         response = os.popen(request).read()
-        print 'Response is: ', response
+        #print 'Response is: ', response
+        return "Success: Create Board"
 
     #returns boards list for user Id
     def getUserBoards(self, userId):
@@ -90,7 +92,7 @@ class DBConn():
     #delete by board name from both tables user boards and board details TODO need to delete all pins for that board
     def deleteBoard(self, userId, boardName):
         prev = self.getBoardDetails(userId,boardName)
-        print ' Prev record for delete: ', prev
+        #print ' Prev record for delete: ', prev
         rev = None
         if "_rev" in prev.keys():
             rev = prev["_rev"]
@@ -98,7 +100,7 @@ class DBConn():
             return "Success: No record found for delete"
         request = 'curl -X DELETE -H "Accept: application/json" http://127.0.0.1:5984/boarddetails/'+boardName+'?rev='+rev
         response = os.popen(request).read()
-        print 'DELETE Board: ', response
+        #print 'DELETE Board: ', response
         #Get previous Board record for this userId from DB
         request = 'curl -X GET -H "Accept: application/json" http://127.0.0.1:5984/userboards/'+str(userId)
         response = os.popen(request).read()
@@ -133,35 +135,48 @@ class DBConn():
             request = "curl -i -H 'Accept: application/json' -H 'Content-Type: application/json' --data '" +json.dumps(data)+ "' http://" + self.host + ":" + str(self.port) + uri
 
         #Execute the DB update request
-        print 'New request is ',request
+        #print 'New request is ',request
         response = os.popen(request).read()
-        print 'Response is: ', response
+        #print 'Response is: ', response
+        return 'Success: Board deleted'
 
     #update board details based on board name, update of board name not supported
-    def updateBoard(self,userId, boardName):
-        data = self.getBoardDetails(self, userId,boardName)
+    def updateBoard(self,userId, boardName, boardDesc, category, isPrivate):
+        data = self.getBoardDetails(userId,boardName)
         #Insert Board Details in table boardDetails
         uri = "/boarddetails"
-        data = {}
+        #data = {}
+        rev = None
+        if "_rev" in data.keys():
+            rev = data["_rev"]
+        else:
+            return 'Failed: Record not found to update'
+
         data["userId"] = userId
-        data["_id"] = boardName
+        #data["_id"] = boardName    #do not change this in update
         data["boardDesc"] = boardDesc
         data["category"] = category
         data["isPrivate"] = isPrivate
         request = "curl -i -H 'Accept: application/json' -H 'Content-Type: application/json' --data '" +json.dumps(data)+ "' http://" + self.host + ":" + str(self.port) + uri
-        print 'New request is ',request
+        #print 'New request is ',request
         response = os.popen(request).read()
-        print 'Response is: ', response
+        #print 'Response is: ', response
 
         return "Success: board updated"
 
 if __name__ == "__main__":
     dbconn = DBConn("127.0.0.1",5984)
-    dbconn.createBoard(1234,"board3","my private board","test",True)
-    res = dbconn.getBoardDetails(1234,"board1")
-    print 'Board details: ',res
-    res = dbconn.getBoardDetails(1234,"board2")
-    print 'Board details: ',res
-    res = dbconn.getBoardDetails(1234,"board3")
-    print 'Board details: ',res
-    res = dbconn.deleteBoard(1234, "board1")
+
+    for i in range(10):
+        dbconn.createBoard(i,"board"+str(i),"my private board","test",True)
+    	res = dbconn.getBoardDetails(i,"board"+str(i))
+    	print 'Board details: ',res
+    	res = dbconn.getBoardDetails(i,"board"+str(i))
+    	print 'Board details: ',res
+    	res = dbconn.getBoardDetails(i,"board"+str(i))
+    	print 'Board details: ',res
+    	res = dbconn.deleteBoard(i, "board"+str(i))
+    	print 'Delete board: ', res
+    	res = dbconn.updateBoard(i,"board"+str(i),"I have been updated","test",True)
+    	print 'Update board: ', res
+
